e35eca94d6ef4b481019cdb5f30d1b6baeafb1be
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-11-09 10:41:55 +0000

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ myDB(async (client) => {
     res.redirect('/profile');
   });
 
-  app.route('/profile').get((req, res) => {
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
     res.render(process.cwd() + '/views/pug/profile');
   });
   
@@ -78,7 +78,13 @@ myDB(async (client) => {
   });
 });
 
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
 
+  res.redirect('/');
+}
 
 
 app.listen(process.env.PORT || 3000, () => {
