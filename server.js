48be22ce8cfe8543fb5fb1bb29d2d147d92964e0
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-11-09 10:15:19 +0000

    Update server.js

--- server.js
+++ server.js
@@ -40,6 +40,18 @@ myDB(async (client) => {
       message: 'Please login'
     });
   });
+  
+  passport.use(new localStrategy(
+  function(username, password, done) {
+    myDatabase.findOne({username:username}, function(err,user) {
+      console.log('User ' + username + ' attempted to log in.');
+      if (err) {return done(err);}
+      if (!user) {return done(null,false);}
+      if (password !== user.password) {return done(null.false);}
+      return done(null,user);
+    });
+  }
+));
 
   // Serialization and deserialization here...
   passport.serializeUser((user, done) => {
