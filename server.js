07ff9b2709c3d4739eddaa185d3b2911d895df6d
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-11-09 10:58:36 +0000

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,15 @@ myDB(async (client) => {
     res.render(process.cwd() + '/views/pug/profile', {username:req.user.username});
   });
   
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+  
+  app.use((req,res,next) => {
+    res.status(404).type('text').send('Not Found');
+  });
+  
   passport.use(new localStrategy(
   function(username, password, done) {
     myDatabase.findOne({username:username}, function(err,user) {
