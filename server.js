9981855284b3d066b2d0a148ac61e55cd943f3c0
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-11-12 10:21:24 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const routes = require('./routes.js');
 const auth = require('./auth.js');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
+const passportSocketIO = require('passport.socketio');
+const MongoStore = require('connect-mongo')(session);
+const cookieParser = require('cookie-parser');
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({url: URI});
 
 const app = express();
 
@@ -24,12 +28,23 @@ app.use(session({
   secret:process.env.SESSION_SECRET,
   resave:true,
   saveUninitialized:true,
-  cookie: {secure:false}
+  cookie: {secure:false},
+  key: 'express.sid',
+  store: store
 }));
 
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+io.use(passportSocketIO.authorize({
+  cookieParser: cookieParser,
+  key: 'express.sid',
+  secret:process.env.SESSION_SECRET,
+  store:store,
+  success: onAuthorizeSuccess,
+  fail: onAuthorizeFail
+}));
 
 
 myDB(async (client) => {
@@ -58,6 +73,19 @@ myDB(async (client) => {
     res.render('pug', { title: e, message: 'Unable to login' });
   });
 });
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
+
 
 
 http.listen(process.env.PORT || 3000, () => {
